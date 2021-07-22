3610f2ba5c5c01dc249f056f73d8d5319317422a
Lizon <Paula.Lizon@lionbridge.com>
2021-07-22 18:37:43 +0200

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAI9caMjgIkXJMrxWde7wcu5KFBYlnmi5M",
+    authDomain: "clone-e3291.firebaseapp.com",
+    projectId: "clone-e3291",
+    storageBucket: "clone-e3291.appspot.com",
+    messagingSenderId: "948243165830",
+    appId: "1:948243165830:web:31d7840128e600fdd2d707",
+    measurementId: "G-ZE9FKWT7K0"
+};
