2f5422b5dcec104f5025d563c1958214965deb34
Lizon <Paula.Lizon@lionbridge.com>
2021-07-29 16:34:58 +0200

    add currencyformat

--- src/components/Subtotal/Subtotal.js
+++ src/components/Subtotal/Subtotal.js
@@ -1,11 +1,30 @@
 import React from "react";
-
+import { Wrapper } from "../Subtotal/Subtotal.styles";
+import CurrencyFormat from "react-currency-format";
 
 const Subtotal = () => {
   return (
-    <>
-I am subtotal component
-    </>
+    <Wrapper>
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items):
+              <strong>0</strong>
+            </p>
+            <small>
+              <input type="checkbox" />
+              This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        // value={getBasketTotal(basket)}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+    </Wrapper>
   );
 };
 
